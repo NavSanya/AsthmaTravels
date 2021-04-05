c7081f5493fb39b34e3e57dfdc7c09a21a7f5e45
NavSanya <57607234+NavSanya@users.noreply.github.com>
2021-04-05 14:12:52 -0700

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -14,5 +14,27 @@ class Graph:
        "Napa":{"Eureka":252, "Sacramento":62, "Oakland":42, "San Fransisco":52},
        "Eureka":{"Redding":147, "Napa":252},
        "Redding":{"Eureka":147, "Sacramento":160}}
+    AQI = []
+    PollenCount=[]
 
+    #for x in f
+    #    AQI.append(x)
+    #for x1 in f1
+    #    PollenCount.append(x1)
+    for i in AQI:
+        def get_all_values(nested_dictionary):
 
+            for key, value in nested_dictionary.items():
+                if type(value) is dict:
+                    get_all_values(value)
+                elif:
+                    distance = value
+                    sensitivityAQI = input("Input Y if you are sensitive to AQI otherwise input anything else")
+                    sensitivityPollen  = input("Input Y if you are sensitive to Pollen otherwise input anything else")
+                    cost = (2*distance) + AQI[i] + PollenCount[i]
+                    if (sensitivityAQI=='Y')
+                        cost+=AQI[i]
+                    if (sensitivityPollen=='Y')
+                        cost+=PollenCount[i]
+                    value = cost
+        
